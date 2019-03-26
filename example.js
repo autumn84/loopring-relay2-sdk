bfc2311b32ccece01554935a1707ad0491a3e5e5
yangli <autumn84@aliyun.com>
2019-03-26 18:50:46 +0800

    add examples

--- example.js
+++ example.js
@@ -1,7 +1,8 @@
-var LoopringRelay = require('./relay2-api');
+var LoopringRelay = require('./node-loopring-relay2-api');
 
-var parameters = {addresses: "0xb94065482ad64d4c2b9252358d746b39e820a582"};
+var request = {address: "0xe20cf871f1646d8651ee9dc95aab1d93160b3467", tokens: []};
+request.tokens.push("0x97241525fe425c90ebe5a41127816dcfa5954b06");
 
 var loopringRelay = new LoopringRelay();
 
-loopringRelay.getAccounts(parameters);
+loopringRelay.getAccounts(10, request);
--- node-loopring-relay2-api.js
+++ node-loopring-relay2-api.js
@@ -4,11 +4,32 @@ const request = require('request');
 const url = require('url');
 
 function LoopringRelay() {
-
-	this.apiUrl = "";
+	// TODO: url待定
+	this.apiUrl = "http://**:8080/api/loopring";
 }
 
-LoopringRelay.prototype.getAccounts = function (requestData, callback = false) {
+
+
+LoopringRelay.prototype.getAccounts = function (requestId, requestData, callback = false) {
+	request({
+		url: this.apiUrl,
+		method: "POST",
+		json: true,
+		headers: {
+	    	"content-type": "application/json",
+		},
+		body : {jsonrpc: "2.0",  id: requestId, method: "get_account", params: requestData}
+	}, function(error, response, body) {
+	    if (!error) {
+	    	console.log(response.statusCode);
+	    	console.log(body.result.accountBalance.tokenBalanceMap);
+	    } else {
+	    	console.error("error: " + error);
+	    }
+	}); 
+};
+
+LoopringRelay.prototype.cancelOrders = function (requestData, callback = false) {
 	request({
 		url: this.apiUrl,
 	    method: "POST",
@@ -16,15 +37,36 @@ LoopringRelay.prototype.getAccounts = function (requestData, callback = false) {
 	    headers: {
 	        "content-type": "application/json",
 	    },
-	    body: JSON.stringify(requestData)
+	    body: {jsonrpc: "2.0", id: "1", method: "cancel_orders", params: requestData}
 	}, function(error, response, body) {
-	    if (!error && response.statusCode == 200) {
-	    	console.log(response);
-	    	console.log(body);
+	    if (!error) {
+	    	console.log(response.statusCode);
+	    	console.log(body.result.accountBalance.tokenBalanceMap);
 	    } else {
-	    	console.error(error);
+	    	console.error("error: " + error);
 	    }
 	}); 
 };
+
+LoopringRelay.prototype.submitOrder = function (requestData, callback = false) {
+	request({
+		url: this.apiUrl,
+	    method: "POST",
+	    json: true,
+	    headers: {
+	        "content-type": "application/json",
+	    },
+	    body: {jsonrpc: "2.0", id: "1", method: "submit_order", params: requestData}
+	}, function(error, response, body) {
+	    if (!error) {
+	    	console.log(response.statusCode);
+	    	console.log(body.result.accountBalance.tokenBalanceMap);
+	    } else {
+	    	console.error("error: " + error);
+	    }
+	}); 
+};
+
+
 
 module.exports = LoopringRelay;
